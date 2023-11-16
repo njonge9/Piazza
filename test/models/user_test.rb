888952f26643a170e2bdd4efdd304b03e63c8d40
Njonge9 <frednjonge9@gmail.com>
2023-11-16 14:06:02 +0300

    Write tests to validte the users model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'requires a name' do
+    @user = User.new(name: '', email: 'johndoe@example.com')
+    assert_not @user.valid?
+
+    @user.name = 'John'
+    assert @user.valid?
+  end
+
+  test 'requires a valid email' do
+    @user = User.new(name: 'John', email: '')
+    assert_not @user.valid?
+
+    @user.email = 'invalid'
+    assert_not @user.valid?
+
+    @user.email = 'johndoe@example.com'
+    assert @user.valid?
+  end
+
+  test 'require a unique email' do
+    @existing_user = User.create(name: 'John', email: 'jdoe@exampe.com')
+    assert @existing_user.persisted?
+
+    @user = User.new(name: 'John', email: 'jdoe@example.com')
+    assert_not @user.valid?
+  end
 end
