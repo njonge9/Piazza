05285414396a4dd76f597382283e3a4762debdf8
Njonge9 <frednjonge9@gmail.com>
2023-12-04 11:43:34 +0300

    Configure the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'feed/show'
-  get 'users/new'
-  get 'users/create'
+  root "feed#show"
+
+  get "sign_up", to: "users#new"
+  get "sign_up", to: "users#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,5 +10,4 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
 end
