8876d80ce4b3739917f766a3df44ec34435bf813
Njonge9 <frednjonge9@gmail.com>
2023-12-05 10:03:03 +0300

    Create a method to store users data in an encrypted cookie

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -1,3 +1,11 @@
 module Authenticate
   extend Activesupport::Concern
+
+  protected
+
+  def log_in(app_session)
+    cookies.encrypted.permanent[:app_session] = {
+      value: app_session.to_h
+    }
+  end
 end
