70216b4d64fe707959684d00c173d08d901f1a2a
Njonge9 <frednjonge9@gmail.com>
2023-12-05 10:02:32 +0300

    Generate a token before the AppSession is saved

--- app/models/app_session.rb
+++ app/models/app_session.rb
@@ -1,3 +1,9 @@
 class AppSession < ApplicationRecord
   belongs_to :user
+
+  has_secure_password :token, validations: false
+
+  before_create {
+    self.token = self.class.generate_unique_secure_token
+  }
 end
