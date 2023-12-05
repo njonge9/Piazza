fb0cdd96c7ce2662f7e39b1539f167e71eab674d
Njonge9 <frednjonge9@gmail.com>
2023-12-05 10:26:02 +0300

    Create the authentication procedure

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -8,4 +8,25 @@ module Authenticate
       value: app_session.to_h
     }
   end
+
+  private
+
+  def authenticate
+    Current.app_session = authenticate_using_cookie
+    Current.user = Current.app_session&.user
+  end
+
+  def authenticate_using_cookie
+    app_session = cookies.encrypted[:app_session]
+    authenticate_using app_session&.with_indifferent_access
+  end
+
+  def authenticate_using(data)
+    data => { user_id:, app_session:, token:}
+
+    user = User.find(user_id)
+    user.authenticate_app_session(app_session, token)
+  rescue NoMatchingPatternError, ActiveRecord::RecordNotFound
+    nil
+  end
 end
