d0d52ad5e3446f8f7682e1f19ffad7e8080fcdc6
Njonge9 <frednjonge9@gmail.com>
2023-12-06 23:45:45 +0300

    Add the targets using stimulus

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  connect() {
+  static targets = ["burger", "menu"]
+
+  toggle() {
+    this.burgerTarget.classList.toggle("is-active")
+    this.menuTarget.classList.toggle("is-active")
   }
 }
