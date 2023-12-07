58fa545d1f6bbc68d80691e5c5ebce5c42502539
Njonge9 <frednjonge9@gmail.com>
2023-12-07 10:25:30 +0300

    Add routes to view and update user details

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+
+  resource :profile, only: [:show, :update], controller: "users"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
