a1c21bfa5758b618763fe75325571382a1130c9e
Njonge9 <frednjonge9@gmail.com>
2023-12-07 12:53:12 +0300

    Retrieve a user using their password reset id

--- app/models/user/password_reset.rb
+++ app/models/user/password_reset.rb
@@ -5,6 +5,20 @@ module User::PasswordReset
     has_secure_password :password_reset_token, validation: false
   end
 
+  class_methods do
+    def find_bypassword_reset_id(id)
+      message_verifier.verified(
+        CGI.unescape(id),
+        purpose: :password_reset
+      )&.symbolize_keys => { user_id:, password_reset_token:}
+
+      User.find(user_id)
+          .authenticate_password_reset_token(password_reset_token)
+      rescue ActiveRecord::RecordNotFound, NoMatchingPatternError
+        nil
+    end
+  end
+
   def reset_password
     update(
       password_reset_token: self.class.generate_unique_secure_token
