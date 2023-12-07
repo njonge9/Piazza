a84a4ebdbc93b07d7199bda52071fad05673f7a8
Njonge9 <frednjonge9@gmail.com>
2023-12-07 13:45:40 +0300

    Specify the development domain for Action Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
+  config.action_mailer.default_url_options = {
+    host: "localhost:3000",
+    protocal: "http"
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
