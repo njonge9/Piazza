d6871f6a17567d891d198dc5be9dde446307c0c7
Njonge9 <frednjonge9@gmail.com>
2023-12-07 12:29:36 +0300

    Handling a password reset

--- app/models/user/password_reset.rb
+++ app/models/user/password_reset.rb
@@ -4,4 +4,21 @@ module User::PasswordReset
   included do
     has_secure_password :password_reset_token, validation: false
   end
+
+  def reset_password
+    update(
+      password_reset_token: self.class.generate_unique_secure_token
+    )
+    app_sessions.destroy_all
+
+    send_password_reset_email
+  end
+
+  private
+
+  def send_password_reset_email
+    UserMailer.with(user: self)
+              .password_reset
+              .deliver_now
+  end
 end
